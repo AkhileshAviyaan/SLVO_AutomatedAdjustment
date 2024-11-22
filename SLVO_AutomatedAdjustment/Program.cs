7cba01e45896a7e29a7d81cc873f02b3872a2fd9
akhilesh.aviyaan@gmail.com <akhilesh.aviyaan@gmail.com>
2024-11-22 23:44:24 +0545

    Add project files.

---
+++ SLVO_AutomatedAdjustment/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClosedXML.Excel;
+
+public class Program()
+{
+	static void Main()
+	{
+		//Console.WriteLine("Give me Excel Path");
+		//string path=Console.ReadLine();
+
+		//Console.WriteLine("Give me Checking Sheet Name");
+		//string checkSheet=Console.ReadLine();
+		
+		//Console.WriteLine("Give me Raw Sheet Name");
+		//string rawSheet=Console.ReadLine();
+
+		string path = "C:\\Users\\1akhi\\OneDrive\\Desktop\\SL\\Book1.xlsm";
+	
+	}
+}
